5ffe0a2ed9acfc352217428a4032e7b701e40718
Chris Bell <chrisbellcodes@gmail.com>
2021-02-06 17:18:15 -0500

    refactor(codes and navbar): Set nav layout and set codes page layout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,19 +2,37 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 
 const Navbar = () => {
+
+  function addToLogo(e) {
+    const logo = document.querySelector('.logo')
+    const chosenNavItem = e.target
+    console.log(chosenNavItem, logo);
+    logo.append(chosenNavItem);
+    
+
+    // On click, if the item is in navitems 
+      // remove item from nav item
+        // add navitem to logo
+
+        //If there is an navitem attached to logo
+          // remove item from logo container
+          // add it back to navitems container
+          // remove logo modifier class
+  }
+
     return (
       <div className="navbar">
-        <h1 className="logo">Chris<spon className="bell">
-            Bell
-        </spon></h1>
+        <h1 className="logo">
+          <span className="logo__piece">Chris</span><span className="logo__piece bell">Bell</span>
+        </h1>
         <div className="navItems">
-          <Link className="navbarItem text-pop" to="/">
+          <Link onClick={addToLogo} className="navbarItem" to="/">
             Codes
           </Link>
-          <Link className="navbarItem" to="/improv">
+          <Link onClick={addToLogo} className="navbarItem" to="/improv">
             Improvises
           </Link>
-          <Link className="navbarItem" to="/misc">
+          <Link onClick={addToLogo} className="navbarItem" to="/misc">
             Does Misc.
           </Link>
         </div>
--- src/pages/Codes.js
+++ src/pages/Codes.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom'
 import me from '../assets/chris-bell.jpeg'
 
 
-
 const Codes = () => {
 
     return(
@@ -15,17 +14,17 @@ const Codes = () => {
                     <div style={{backgroundImage: 'url(' + me + ')'}} className="headshot"></div>
                 </div>
                 <div className="about-content">
-                    <h2>About</h2>
-                    <p>Deadlights jack lad schooner scallywag dance the hempen jig carouser broadside cable strike colors. Bring a spring upon her cable holystone blow the man down spanker Shiver me timbers to go on account lookout wherry doubloon chase. Belay yo-ho-ho keelhaul squiffy black spot yardarm spyglass sheet transom heave to.</p>
+                    <h2 className="header" >About</h2>
+                    <p className="body-text">Deadlights jack lad schooner scallywag dance the hempen jig carouser broadside cable strike colors. Bring a spring upon her cable holystone blow the man down spanker Shiver me timbers to go on account lookout wherry doubloon chase. Belay yo-ho-ho keelhaul squiffy black spot yardarm spyglass sheet transom heave to.</p>
                 </div>
             </div>
 
-            <div>
-                <div><Link className="content-list" to={`/codes-projects`}>Skills & Projects</Link></div>
-                <div><Link className="content-list" to={`/codes-resume`}>Resume</Link></div>
-                <div><Link className="content-list" to={`/codes-edu`}>Education</Link></div>
-                <div><a className="content-list" target="_blank" rel='noreferrer' href="https://www.linkedin.com/in/letchrisbellring/">LinkedIn</a></div>
-                <div><a className="content-list" target="_blank" rel='noreferrer' href="https://github.com/chrisbellcodes">GitHub</a></div>
+            <div className="section-nav">
+                <Link className="content-list" to={`/codes-projects`}>Skills & Projects</Link>
+                <Link className="content-list" to={`/codes-resume`}>Resume</Link>
+                <Link className="content-list" to={`/codes-edu`}>Education</Link>
+                <a className="content-list" target="_blank" rel='noreferrer' href="https://www.linkedin.com/in/letchrisbellring/">LinkedIn</a>
+                <a className="content-list" target="_blank" rel='noreferrer' href="https://github.com/chrisbellcodes">GitHub</a>
             </div>
         </div>
     )
